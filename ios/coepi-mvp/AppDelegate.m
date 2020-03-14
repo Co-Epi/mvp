44e796a18367f431e5d861bf4f77230c56716f7c
Ryan Bennett <ry.nomad@protonmail.com>
2020-03-13 21:28:52 -0600

    copy in AppDelegate

--- ios/coepi-mvp/AppDelegate.m
+++ ios/coepi-mvp/AppDelegate.m
@@ -2,6 +2,10 @@
 
 #import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
